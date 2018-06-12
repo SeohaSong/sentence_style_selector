c36389ef731ebcdc7cd69ee5228bfd3aeb64358c
seohasong <tisutoo@gmail.com>
2018-06-12 18:17:11 +0900

    update origin master

--- module_/utils/tf_agent.py
+++ module_/utils/tf_agent.py
@@ -42,16 +42,16 @@ class TF_Agent():
     
         def get_X(sen):
             X = np.zeros(64, dtype=np.int32)-1
-            X[-len(sen):] = sen
+            X[:len(sen)] = sen
             return X
 
-        def get_y(label):
+        def get_y_label(label):
             idx = ['neg', 'pos'].index(label)
             y = np.array([0, 0])
             y[idx] = 1
             return y
         
-        def get_y_isreal(label):
+        def get_y_valid(label):
             idx = ['fake', 'real'].index(label)
             y = np.array([0, 0])
             y[idx] = 1
@@ -61,13 +61,13 @@ class TF_Agent():
         df = df[start:end]
         
         Xs = np.array([get_X(sen) for sen in df['pos']])
-        ys = np.array([get_y(label) for label in df['label']])
-        y_valids = np.array([get_y_isreal(label) for label in df['valid']])
+        y_labels = np.array([get_y_label(label) for label in df['label']])
+        y_valids = np.array([get_y_valid(label) for label in df['valid']])
         
         if n_batch == i+1:
             self.n_epoch += 1
 
-        return Xs, ys, y_valids
+        return Xs, y_labels, y_valids
     
     def convet_a2s(self, array):
 
@@ -94,7 +94,7 @@ class TF_Agent():
         sen = ' '.join([i2w[i].split("-")[0] for i in l])
         return sen
 
-    def get_graph(self):
+    def get_graph_wake(self):
         
         table = self.table
 
@@ -102,69 +102,89 @@ class TF_Agent():
         graph = tf.get_default_graph()
 
         X = tf.placeholder(dtype=tf.int32, shape=[None, 64])
-        y = tf.placeholder(dtype=tf.float32, shape=[None, 2])
+        y_label = tf.placeholder(dtype=tf.float32, shape=[None, 2])
         y_valid = tf.placeholder(dtype=tf.float32, shape=[None, 2])
+        style = tf.placeholder(dtype=tf.float32, shape=[None, 2])
 
         lookup_table = tf.Variable(table, dtype=tf.float32)
         X_ebd = tf.nn.embedding_lookup(lookup_table, X)
 
-        lstm_cell_fw = tf.nn.rnn_cell.BasicLSTMCell(128)
-        lstm_cell_bw = tf.nn.rnn_cell.BasicLSTMCell(128)
-        output, _ = tf.nn.bidirectional_dynamic_rnn(
+        lstm_cell_fw = tf.nn.rnn_cell.BasicLSTMCell(256)
+        lstm_cell_bw = tf.nn.rnn_cell.BasicLSTMCell(256)
+        outputs, _ = tf.nn.bidirectional_dynamic_rnn(
             lstm_cell_fw, lstm_cell_bw, X_ebd, dtype=tf.float32
         )
-        logit = tf.concat(output, axis=1)
+        output = tf.concat(outputs, axis=1)
 
-        ############ for lebal ##########
-        conv = tf.reshape(tf.layers.conv1d(logit, 128, 128), [-1, 128])
+        conv = tf.reshape(tf.layers.conv1d(output, 128, 256), [-1, 256])
         conv_bn = tf.layers.batch_normalization(conv)
         conv_sg = tf.nn.sigmoid(conv_bn)
-        conv_fin = tf.layers.dropout(conv_sg, 0.2)
+        latent = tf.layers.dropout(conv_sg, 0.2)
 
-        w = tf.Variable(tf.truncated_normal([128, 2], dtype=tf.float32))
-        b = tf.Variable(tf.truncated_normal([2], dtype=tf.float32))
-        fin = tf.nn.softmax(tf.matmul(conv_fin, w)+b)
+        ############ for wake ##########
+        w0_label = tf.Variable(tf.truncated_normal([256, 64],
+                                                   dtype=tf.float32))
+        b0_label = tf.Variable(tf.truncated_normal([64],
+                                                   dtype=tf.float32))
+        a0_label = tf.nn.softmax(tf.matmul(latent, w0_label)+b0_label)
+        d0_label = tf.layers.dropout(a0_label, 0.2)
+        w1_label = tf.Variable(tf.truncated_normal([64, 2],
+                                                   dtype=tf.float32))
+        b1_label = tf.Variable(tf.truncated_normal([2],
+                                                   dtype=tf.float32))
+        a1_label = tf.nn.softmax(tf.matmul(d0_label, w1_label)+b1_label)
+        
+        w0_valid = tf.Variable(tf.truncated_normal([256, 64],
+                                                   dtype=tf.float32))
+        b0_valid = tf.Variable(tf.truncated_normal([64],
+                                                   dtype=tf.float32))
+        a0_valid = tf.nn.softmax(tf.matmul(latent, w0_valid)+b0_valid)
+        d0_valid = tf.layers.dropout(a0_label, 0.2)
+        w1_valid = tf.Variable(tf.truncated_normal([64, 2],
+                                                   dtype=tf.float32))
+        b1_valid = tf.Variable(tf.truncated_normal([2],
+                                                   dtype=tf.float32))
+        a1_valid = tf.nn.softmax(tf.matmul(d0_valid, w1_valid)+b1_valid)
 
         acc_label = tf.reduce_mean(tf.cast(
-            tf.equal(tf.argmax(fin, axis=1), tf.argmax(y, axis=1)),
+            tf.equal(tf.argmax(a1_label, axis=1), tf.argmax(y_label, axis=1)),
             dtype=tf.float32
         ))
-        loss_label = tf.losses.softmax_cross_entropy(y, fin)
-        ############ for lebal ##########
-
-        ########### for valid ###########
-        conv_valid = tf.reshape(tf.layers.conv1d(logit, 128, 128), [-1, 128])
-        conv_bn_valid = tf.layers.batch_normalization(conv_valid)
-        conv_sg_valid = tf.nn.sigmoid(conv_bn_valid)
-        conv_fin_valid = tf.layers.dropout(conv_sg_valid, 0.2)
-
-        w_valid = tf.Variable(tf.truncated_normal([128, 2], dtype=tf.float32))
-        b_valid = tf.Variable(tf.truncated_normal([2], dtype=tf.float32))
-        fin_valid = tf.nn.softmax(tf.matmul(conv_fin_valid, w_valid)+b_valid)
-
         acc_valid = tf.reduce_mean(tf.cast(
-            tf.equal(tf.argmax(fin_valid, axis=1), tf.argmax(y_valid, axis=1)),
+            tf.equal(tf.argmax(a1_valid, axis=1), tf.argmax(y_valid, axis=1)),
             dtype=tf.float32
         ))
-        loss_valid = tf.losses.softmax_cross_entropy(y_valid, fin_valid)
-        ########### for valid ###########
 
-        ############## for gen ##########
+        loss_label = tf.losses.softmax_cross_entropy(y_label, a1_label)        
+        loss_valid = tf.losses.softmax_cross_entropy(y_valid, a1_valid)
+        ############ for wake ##########
+
+        ############ for sleep #########
+        output_l = tf.concat([
+            tf.reshape(latent, shape=[-1, 1, 256]),
+            output
+        ], axis=1)
+        output_s = tf.concat([
+            tf.zeros(shape=[129, 2])+style,
+            output_l
+        ], axis=2)
+
         lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=512)
         output_gen, _ = tf.nn.dynamic_rnn(
-            lstm_cell, logit, dtype=tf.float32
+            lstm_cell, output_s, dtype=tf.float32
         )
-
-        w_gen = tf.Variable(tf.truncated_normal([64, 128], dtype=tf.float32))
+        
+        w_gen = tf.Variable(tf.truncated_normal([64, 128+1], dtype=tf.float32))
         b_gen = tf.Variable(tf.truncated_normal([64, 512], dtype=tf.float32))
-        fin_gen = tf.map_fn(lambda x: tf.matmul(w_gen, x)+b_gen, output_gen)
+        f_gen = tf.map_fn(lambda x: tf.matmul(w_gen, x)+b_gen, output_gen)
+        sentence = f_gen
 
-        loss_gen = tf.losses.mean_squared_error(X_ebd, fin_gen)
-        ############## for gen ##########
+        loss_gen = tf.losses.mean_squared_error(X_ebd, f_gen)
+        ############ for sleep #########
 
         loss = loss_label+loss_valid+loss_gen
-        learn = tf.train.AdamOptimizer().minimize(loss)
         loss_total = loss
+        learn = tf.train.AdamOptimizer().minimize(loss)
 
         initilizer = tf.global_variables_initializer()
 
