78a3b4e91f478094835bffa89e484571d1f0501c
seohasong <tisutoo@gmail.com>
2018-06-16 15:36:18 +0900

    update origin master

--- main.py
+++ main.py
@@ -252,12 +252,12 @@ class SSS():
         self.acc_l, self.acc_v = acc_l, acc_v
         self.learn_w = tf.train.AdamOptimizer().minimize(loss_w)
 
-        # loss_l_s, loss_v_s, acc_l_s, acc_v_s = wake(
-        #     get_latent(get_output(gen)), y_label_s, y_valid
-        # )
+        loss_l_s, loss_v_s, acc_l_s, acc_v_s = wake(
+            get_latent(get_output(gen)), y_label_s, y_valid
+        )
         # loss_s = loss_l_s+loss_v_s*10+loss_gen
         loss_s = loss_gen
-        # self.acc_l_s, self.acc_v_s = acc_l_s, acc_v_s
+        self.acc_l_s, self.acc_v_s = acc_l_s, acc_v_s
         self.learn_s = tf.train.AdamOptimizer().minimize(loss_s)
 
         self.initializer = tf.global_variables_initializer()
